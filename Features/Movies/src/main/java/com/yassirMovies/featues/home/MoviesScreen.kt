dfe53434482f945bd24114d0ae97f1f5d158d6d0
Ibrahim Fakharany <ibraheemelfakharany@gmail.com>
2023-08-28 15:33:14 +0300

    Enhancing the error view when error in refreshing the data

--- Features/Movies/src/main/java/com/yassirMovies/featues/home/MoviesScreen.kt
+++ Features/Movies/src/main/java/com/yassirMovies/featues/home/MoviesScreen.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,7 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemsIndexed
+import androidx.paging.compose.items
 import com.ramcosta.composedestinations.annotation.Destination
 import com.yassirMovies.base.MovieItem
 
@@ -34,46 +38,85 @@ fun HomeScreen(navigator: HomeScreenNavigator, viewModel: MoviesViewModel = hilt
     Box(
         modifier = Modifier.fillMaxSize().background(
             Color.White
-        )
+        ), contentAlignment = Alignment.Center
     ) {
-        if (movies.loadState.refresh is LoadState.Loading) {
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
-            )
-        } else if (movies.loadState.refresh is LoadState.Error) {
-            Text(
-                (movies.loadState.refresh as LoadState.Error).error.message.toString(),
-                modifier = Modifier.align(Alignment.Center)
-            )
-        } else {
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                itemsIndexed(movies) { index, movie ->
-                    movie?.let {
-                        MovieItem(modifier = Modifier.clickable {
-                            navigator.navigateToMovieDetails(movie.id)
-                        }, movie = movie)
-                    }
-
-                    if (index < movies.itemSnapshotList.lastIndex) {
-                        Spacer(modifier = Modifier.height(14.dp))
-                        Divider(
-                            color = Color.Black.copy(
-                                alpha = .2f
-                            ), thickness = 1.dp
-                        )
-                    }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(items = movies, key = { post ->
+                post.id
+            }) { item ->
+                item?.let { movie ->
+                    MovieItem(modifier = Modifier.clickable {
+                        navigator.navigateToMovieDetails(movie.id)
+                    }, movie = movie)
+                    Spacer(modifier = Modifier.size(20.dp))
                 }
-                item {
-                    if (movies.loadState.append is LoadState.Loading) {
-                        CircularProgressIndicator()
-                    }
+
+            }
+            if (movies.loadState.refresh is LoadState.Error && movies.itemCount > 0) item {
+                AppendErrorScreen {
+                    movies.retry()
                 }
             }
+            when (movies.loadState.append) {
+                is LoadState.NotLoading -> Unit
+                is LoadState.Loading -> item {
+                    CircularProgressIndicator()
+                }
+
+                is LoadState.Error -> item {
+                    AppendErrorScreen {
+                        movies.retry()
+                    }
+                }
+
+            }
         }
+        when (movies.loadState.refresh) {
+            is LoadState.Loading -> {
+                if (movies.itemCount == 0) CircularProgressIndicator()
+            }
+
+            is LoadState.Error -> {
+                if (movies.itemCount == 0) RefreshErrorScreen { movies.retry() }
+            }
+
+            else -> Unit
+        }
+
+
     }
 
+}
+
+
+@Composable
+fun RefreshErrorScreen(retry: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("There is no internet connection")
+        Button(onClick = {
+            retry.invoke()
+
+        }) {
+            Text("Retry")
+        }
+    }
+}
+
+@Composable
+fun AppendErrorScreen(retry: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text("There is no internet connection")
+        Button(onClick = {
+            retry.invoke()
+        }) {
+            Text("Retry")
+        }
+    }
 }
